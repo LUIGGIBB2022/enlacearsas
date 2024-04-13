b3814597f48e4e48eba4d02ed2c34d22e39a42ee
LUIGGIBB2022 <enlacevisualcis@gmail.com>
2024-04-13 07:41:21 -0500

    Consolidated Sales Center

--- app/Http/Controllers/api/VentasController.php
+++ app/Http/Controllers/api/VentasController.php
@@ -483,6 +483,7 @@ class VentasController extends Controller
             DB::raw('centrooperativo.nombre as centrodeoperacion'),
             DB::raw('sum(totalfactura) as totalventas'),
             DB::raw("DATE_FORMAT(fechafactura,'%M %Y') as months")
+            DB::raw("DATE_FORMAT(fechafactura,'%m') as mes")
         )
             ->leftjoin('centrooperativo', 'facturas.centrooper', '=', 'centrooperativo.codigo')
             ->where('facturas.estado','=',1)
