f739861655935e251c85befda42c6c26950d0abc
LUIGGIBB2022 <enlacevisualcis@gmail.com>
2024-04-19 07:11:19 -0500

    Updated Sales Balance

--- app/Http/Controllers/api/VentasController.php
+++ app/Http/Controllers/api/VentasController.php
@@ -481,7 +481,7 @@ class VentasController extends Controller
         $anop  = $request->año;
         $ventas = factura::select(
             DB::raw('centrooperativo.nombre as centrodeoperacion'),
-            DB::raw("sum(totalfactura) as totalventas"),
+            DB::raw("sum(int(totalfactura)) as totalventas"),
             DB::raw("DATE_FORMAT(fechafactura,'%M %Y') as months"),
             DB::raw("DATE_FORMAT(fechafactura,'%m') as mes")
         )
