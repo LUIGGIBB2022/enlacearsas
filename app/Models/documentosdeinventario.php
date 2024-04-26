2b74b2bcbfbefdc36df6c69a4eeda636ffc5ba79
LUIGGIBB2022 <enlacevisualcis@gmail.com>
2024-04-26 11:51:56 -0500

    Update Sales Dctos

--- app/Models/documentosdeinventario.php
+++ app/Models/documentosdeinventario.php
@@ -14,7 +14,7 @@ class documentosdeinventario extends Model
         'valordocumento','descuentoproductos','descuentoadicional','retefuente','reteiva','reteica','otrasret1','otrasret2','otrasret3','otrasret4','otrasret5',
         'totaldocumento','valoradicional','valoriva','ordendecompra','placa','proyecto','sproyecto','centrooper','centro','scentro','actividad','lapso','tipodemovimiento',
         'tipodecompra','conceptotraslado','cptoclasetraslado','estado','estado01','estado02','estado03','bodegatraslado','lotetraslado','ivaasumido','observaciones',
-        'documentosdeinventariosID','usuario_created','usuario_updated','fecha_created','fecha_updated'];
+        'documentosdeinventariosID','cufe','msgevento1','msgevento2','msgevento3','fechavento1','fechavento2','fechavento3','codstatus','copdestino','usuario_created','usuario_updated','fecha_created','fecha_updated'];
 
     protected $primaryKey = "documentosdeinventariosID";
     public $timestamps = false;
