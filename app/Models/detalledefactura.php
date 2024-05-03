2fe14e014c4620e3fb9c5f4d954356f3415465af
LUIGGIBB2022 <enlacevisualcis@gmail.com>
2024-05-02 20:16:08 -0500

    Update Sales Enlace

--- app/Models/detalledefactura.php
+++ app/Models/detalledefactura.php
@@ -15,7 +15,7 @@ class detalledefactura extends Model
         'numerodefactura','tipodedocumento','prefijo','nit','sucursal','fechadefactura','fechadevencimiento','proyecto','sproyecto','centrooper','cuenta','centro','scentro','lapso','tipodemovimiento',
         'producto','producto2','descripcion','bodega','lote','cantidad','cantidad1','valorventa','costopromedio','porcentajeiva','descuento1','descuento2','descuento3','valordescuento1','valordescuento2','valordescuento3',
         'idregistro','impoconsumo','concepto','cptoclase','garantia','serial','propiedad','tipocliente','rutadeventa','zonadeventa','vendedor','tecnico','facturasid','clientesid','estado','estado01','estado02',
-        'estado03','vehiculo','usuario_created','usuario_updated','fecha_created','fecha_updated'];
+        'estado03','idlocal','vehiculo','usuario_created','usuario_updated','fecha_created','fecha_updated'];
 
     protected $primaryKey = "detalledefacturasid";
     public $timestamps = false;
